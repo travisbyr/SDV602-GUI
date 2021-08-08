3d6470bc7e51f1389ca7b6e9e216094bcfbd2124
Travis Byrman <travis-byrman@live.nmit.ac.nz>
2021-08-09 10:35:31 +1200

    Fixed spacing

--- src/AdventureGameGUI.py
+++ src/AdventureGameGUI.py
@@ -1,12 +1,12 @@
 import PySimpleGUI as sg
 
 layout = [[sg.Text("You are in a forest, where do you want to go?", text_color="#343434", background_color="#f2f2f2", key='-head-')],
-          # [sg.Input(key='-INPUT-')],
-          [sg.Text(size=(40, 1), background_color="#f2f2f2", key='-OUTPUT-')],
-          [sg.Button('Left', button_color=('black', 'orange')), sg.Button('Right', button_color=('black', 'red')), sg.Button('Quit', button_color=('black', 'yellow'))]]
+        # [sg.Input(key='-INPUT-')],
+        [sg.Text(size=(40, 1), background_color="#f2f2f2", key='-OUTPUT-')],
+        [sg.Button('Left', button_color=('black', 'orange')), sg.Button('Right', button_color=('black', 'red')), sg.Button('Quit', button_color=('black', 'yellow'))]]
 
 window = sg.Window('Adventure Game', auto_size_text=True,
-                   background_color="#f2f2f2", default_element_size=(40, 1)).Layout(layout)
+            background_color="#f2f2f2", default_element_size=(40, 1)).Layout(layout)
 #userInput = window['-INPUT-']
 header = window['-head-']
 
