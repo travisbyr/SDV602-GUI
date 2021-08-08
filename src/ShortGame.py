00782cbc04f2ff1b8abb64feff8b50d33515a19a
Travis Byrman <travis-byrman@live.nmit.ac.nz>
2021-08-09 10:35:50 +1200

    Added short game using if statements

---
+++ src/ShortGame.py
@@ -0,0 +1,139 @@
+print("------------------------------------------------")
+print("You are at a forest, where do you want to go?")
+print("------------------------------------------------")
+print("Left go to to the castle")
+print("Right to go to the house")
+print("------------------------------------------------")
+str = input("Where do you want to go? ")
+print("------------------------------------------------")
+
+if str == "Left":
+    print("You are now at a castle, where do you want to go?")
+    print("------------------------------------------------")
+    print("Left go to to the machine shop")
+    print("Right to go to the butchery")
+    print("------------------------------------------------")
+    str = input("Where do you want to go? ")
+    print("------------------------------------------------")
+
+    if str == "Left":
+        print("You are now at a machine shop, where do you want to go?")
+        print("------------------------------------------------")
+        print("Left go to to the house")
+        print("Right to go to the bakery")
+        print("------------------------------------------------")
+        str = input("Where do you want to go? ")
+        print("------------------------------------------------")
+
+        if str == "Right":
+            print("You are now at a machine shop, where do you want to go?")
+            print("------------------------------------------------")
+            print("Left go to to the house")
+            print("Right to go to the bakery")
+            print("------------------------------------------------")
+            str = input("Where do you want to go? ")
+            print("------------------------------------------------")
+
+            if str == "Right":
+                print("You are now at a machine shop, where do you want to go?")
+                print("------------------------------------------------")
+                print("Left go to to the house")
+                print("Right to go to the bakery")
+                print("------------------------------------------------")
+                str = input("Where do you want to go? ")
+                print("------------------------------------------------")
+
+                if str == "Right":
+                    print("You are now at a machine shop, where do you want to go?")
+                    print("------------------------------------------------")
+                    print("Left go to to the house")
+                    print("Right to go to the bakery")
+                    print("------------------------------------------------")
+                    str = input("Where do you want to go? ")
+                    print("------------------------------------------------")
+
+                    if str == "Right":
+                        print(
+                            "You are now at a machine shop, where do you want to go?")
+                        print("------------------------------------------------")
+                        print("Left go to to the house")
+                        print("Right to go to the bakery")
+                        print("------------------------------------------------")
+                        str = input("Where do you want to go? ")
+                        print("------------------------------------------------")
+
+                        if str == "Right":
+                            print(
+                                "You are now at a machine shop, where do you want to go?")
+                            print("------------------------------------------------")
+                            print("Left go to to the house")
+                            print("Right to go to the bakery")
+                            print("------------------------------------------------")
+                            str = input("Where do you want to go? ")
+                            print("------------------------------------------------")
+
+
+if str == "Right":
+    print("You are now at a butchery, where do you want to go?")
+    print("------------------------------------------------")
+    print("Left go to to the shack")
+    print("Right to go to the bakery")
+    print("------------------------------------------------")
+    str = input("Where do you want to go? ")
+    print("------------------------------------------------")
+
+    if str == "Left":
+        print("You are now at a butchery, where do you want to go?")
+        print("------------------------------------------------")
+        print("Left go to to the shack")
+        print("Right to go to the bakery")
+        print("------------------------------------------------")
+        str = input("Where do you want to go? ")
+        print("------------------------------------------------")
+
+        if str == "Right":
+            print("You are now at a butchery, where do you want to go?")
+            print("------------------------------------------------")
+            print("Left go to to the shack")
+            print("Right to go to the bakery")
+            print("------------------------------------------------")
+            str = input("Where do you want to go? ")
+            print("------------------------------------------------")
+
+            if str == "Right":
+                print("You are now at a machine shop, where do you want to go?")
+                print("------------------------------------------------")
+                print("Left go to to the house")
+                print("Right to go to the bakery")
+                print("------------------------------------------------")
+                str = input("Where do you want to go? ")
+                print("------------------------------------------------")
+
+                if str == "Right":
+                    print("You are now at a machine shop, where do you want to go?")
+                    print("------------------------------------------------")
+                    print("Left go to to the house")
+                    print("Right to go to the bakery")
+                    print("------------------------------------------------")
+                    str = input("Where do you want to go? ")
+                    print("------------------------------------------------")
+
+                    if str == "Right":
+                        print(
+                            "You are now at a machine shop, where do you want to go?")
+                        print("------------------------------------------------")
+                        print("Left go to to the house")
+                        print("Right to go to the bakery")
+                        print("------------------------------------------------")
+                        str = input("Where do you want to go? ")
+                        print("------------------------------------------------")
+
+                        if str == "Right":
+                            print(
+                                "You are now at a machine shop, where do you want to go?")
+                            print("------------------------------------------------")
+                            print("Left go to to the house")
+                            print("Right to go to the bakery")
+                            print("------------------------------------------------")
+                            str = input("Where do you want to go? ")
+                            print("------------------------------------------------")
